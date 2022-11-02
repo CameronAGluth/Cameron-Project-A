bd0044950b532aeea288793c23269b9a41d4849b
CameronAGluth <90208438+CameronAGluth@users.noreply.github.com>
2022-11-02 14:04:49 -0500

    working on javascript import

---
+++ JavaScript/main.js
@@ -0,0 +1,7 @@
+// jQuery 3.x-style ready event and locally scoped $
+// jQuery(function($) {
+//   $('html').removeClass('nojs');
+//   $('html').addClass('hasjs');
+// });
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+console.log("Hello World!")
