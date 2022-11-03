9ff619d4a52ecbfbf7cb4e7f217ba63fc38dd640
CameronAGluth <90208438+CameronAGluth@users.noreply.github.com>
2022-11-03 18:11:40 -0500

    worked on JavaScript

--- script/main.js
+++ script/main.js
@@ -3,5 +3,15 @@
 //   $('html').removeClass('nojs');
 //   $('html').addClass('hasjs');
 // });
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
+// document.getElementById("demo").innerHTML = "Hello JavaScript!";
 console.log("Hello World!");
+
+function validate () {
+  //Email
+  // document.getElementById("email")
+  let x = document.forms["form"]["email"].value;
+  console.log(x);
+
+  //18+
+  // document.getElementById("age-verification")
+}
