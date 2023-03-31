11d0be84b01df61da442bef2a62c7912a967be50
CameronG <cgluth@hawk.iit.edu>
2023-03-30 23:30:06 -0500

    working on javascript

--- script/main.js
+++ script/main.js
@@ -4,14 +4,33 @@
 //   $('html').addClass('hasjs');
 // });
 // document.getElementById("demo").innerHTML = "Hello JavaScript!";
+// function validate () {
+//   //Email
+//   // document.getElementById("email")
+//   let x = document.forms["form"]["email"].value;
+//   console.log(x);
+//
+//   //21+
+//   // document.getElementById("age-verification")
+//   let y = document.forms["form"]["age"].value;
+//   console.log(y);
+//   if age < 21 {
+//
+//   }
+// }
+
 console.log("Hello World!");
-
-function validate () {
-  //Email
-  // document.getElementById("email")
-  let x = document.forms["form"]["email"].value;
-  console.log(x);
-
-  //21+
-  // document.getElementById("age-verification")
+}
+function verify() {
+  var cbox = document.forms["form"]["employee-verification"]["age-verification"];
+  if (
+    cbox[0].checked == false &&
+    cbox[1].checked == false &&
+  ) {
+    alert("Please ensure that you check BOTH checkboxes");
+    return false;
+  } else {
+    alert("Thank you for submitting your waiver. We can't wait to see you at the employee party on June 12!");
+    return true;
+  }
 }
